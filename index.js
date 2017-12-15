c431e151e062b02d5b69662371c3ff8f5ac72126
Chris Allick <chrisallick@gmail.com>
2017-12-15 10:12:35 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+
+const fetch = require("node-fetch");
+const shopify = require("shopify-buy");
+const express = require('express');
+const app = express();
+
+const client = require('twilio')(process.env.TWILIO_API_KEY, process.env.TWILIO_API_SECRET);
+
+const shopifyclient = shopify.buildClient({
+	storefrontAccessToken: process.env.SHOPIFY_TOKEN,
+	domain: process.env.SHOPIFY_DOMAIN
+}, fetch);
+
+function checkSoldOut() {
+	if( total_product_types_available == total_product_types ) {
+		console.log("we are not sold out!");
+	} else if( total_product_types_available != total_product_types && total_product_types_available > 0  ) {
+		console.log("something sold out, but not everything");
+	} else if( total_product_types_available == 0 ) {
+		console.log("we are sold out of everything");
+		client.messages.create({
+			from: process.env.TWILIO_FROM,
+			to: process.env.TWILIO_TO,
+			body: "the store is sold out"
+		}).then((messsage) => console.log(message.sid));
+	}
+}
+
+var total_product_types;
+var total_product_types_available;
+app.get('/', (req, res) => {
+	shopifyclient.product.fetchAll().then((products) => {
+		total_product_types = products.length;
+		total_product_types_available = 0;
+		var current = 0;
+		for(var i = 0; i < products.length; i++ ) {
+
+			shopifyclient.product.fetch(products[i].id).then((product) => {
+				if( product.variants[0].available ) {
+					total_product_types_available++;
+				}
+
+				current++;
+				if( current == total_product_types ) {
+					checkSoldOut();
+				}
+			});
+		}
+	});
+
+	res.send('Checking if store is sold out...');
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
